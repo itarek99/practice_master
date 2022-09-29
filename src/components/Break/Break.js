3eb01ee4b947bd4cf6110661a811d92e27db297b
itarek99 <tarekulislam7060@gmail.com>
2022-09-29 21:30:51 +0600

    break complete

--- src/components/Break/Break.js
+++ src/components/Break/Break.js
@@ -1,4 +1,17 @@
+import './Break.css';
+
 function Break() {
-  return <div>Break</div>;
+  const breakTimes = [2, 5, 10, 15, 20];
+
+  return (
+    <div className='break'>
+      <h3 className='break__title'>Add A Break</h3>
+      <div className='break__times'>
+        {breakTimes.map((time) => (
+          <button className='break__time'>{time}m</button>
+        ))}
+      </div>
+    </div>
+  );
 }
 export default Break;
